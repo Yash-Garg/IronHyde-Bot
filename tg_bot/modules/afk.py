4e7d5135f32530ecfe2aa7f4b35f6e056599d1ac
krypticallusion <krypticallusion@gmail.com>
2018-05-14 10:39:44 +0000

    Check if afk even when someone replies
    Signed-off-by: krypticallusion <krypticallusion@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -58,13 +58,22 @@ def reply_afk(bot: Bot, update: Update):
             else:
                 return
 
-            if sql.is_afk(user_id):
-                user = sql.check_afk_status(user_id)
-                if not user.reason:
-                    res = "{} is AFK!".format(fst_name)
-                else:
-                    res = "{} is AFK! says its because of:\n{}".format(fst_name, user.reason)
-                message.reply_text(res)
+            check_afk(bot, update, user_id, fst_name)
+
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        fst_name = message.reply_to_message.from_user.first_name
+        check_afk(bot, update, user_id, fst_name)
+
+def check_afk(bot, update, user_id, fst_name):
+    if sql.is_afk(user_id):
+        user = sql.check_afk_status(user_id)
+        if not user.reason:
+            res = "{} is AFK!".format(fst_name)
+        else:
+            res = "{} is AFK! says its because of: \n{}".format(fst_name, user.reason)
+        update.effective_message.reply_text(res)
+
 
 
 __help__ = """
@@ -78,9 +87,8 @@ __mod_name__ = "AFK"
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
 AFK_REGEX_HANDLER = DisableAbleRegexHandler("(?i)brb", afk, friendly="afk")
-NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
-AFK_REPLY_HANDLER = MessageHandler(Filters.entity(MessageEntity.MENTION) | Filters.entity(MessageEntity.TEXT_MENTION),
-                                   reply_afk)
+NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group , no_longer_afk)
+AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group , reply_afk)
 
 dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
