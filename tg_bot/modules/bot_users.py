c450739d402fc29d425454efe60dd1477b1ccbb0
lzzy12 <jhashivam2020@gmail.com>
2018-06-14 07:53:19 +0000

    Add handlers for listsudo

--- tg_bot/modules/bot_users.py
+++ tg_bot/modules/bot_users.py
@@ -1,16 +1,21 @@
 import html
-from io import BytesIO
 from telegram import Message, Update, Bot, User, Chat, ParseMode
 from telegram.error import BadRequest, TelegramError
 from telegram.ext import run_async, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 from tg_bot import dispatcher, OWNER_ID, SUDO_USERS
+import tg_bot.modules.sql.users_sql as sql
 
 @run_async
 def listsudo(bot: Bot, update: Update):
     message = update.effective_message
     reply_msg = "**SUDO USERS:**"
     for i in SUDO_USERS:
-        reply_msg += "\n" + i
+       reply_msg += "\n" + str(i)
 
     message.reply_text(reply_msg)
+    return
+
+__mod_name__ = "Bot users"
+BOTUSER_HANDLER = CommandHandler("listsudo", listsudo)
+dispatcher.add_handler(BOTUSER_HANDLER)
