ae6d7bf10bc865b5b3a7c501d93aa64723ca2031
Yash Garg <ben10.yashgarg@gmail.com>
2018-06-25 17:14:58 +0530

    Join args rather than taking the first object & Replace some functions to the inbuilt math api
    Authored-by: krypticallusion <krypticallusion@gmail.com>
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>

--- tg_bot/modules/math.py
+++ tg_bot/modules/math.py
@@ -1,90 +1,93 @@
 from typing import List
-
 import requests
 from telegram import Message, Update, Bot, MessageEntity
 from telegram.ext import CommandHandler, run_async
-import pynewtonmath as newton
 from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
+import pynewtonmath as newton
+import math
+
+def join(args):
+    x = ''.join(map(str, args))
+    return x
 
 @run_async
 def simplify(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
-    message.reply_text(newton.simplify('{}'.format(args[0])))
+    message.reply_text(newton.simplify('{}'.format(join(args))))
 
 @run_async
 def factor(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
-    message.reply_text(newton.factor('{}'.format(args[0])))
+    message.reply_text(newton.factor('{}'.format(join(args))))
 
 @run_async
 def derive(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
-    message.reply_text(newton.derive('{}'.format(args[0])))
+    message.reply_text(newton.derive('{}'.format(join(args))))
 
 @run_async
 def integrate(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
-    message.reply_text(newton.integrate('{}'.format(args[0])))
+    message.reply_text(newton.integrate('{}'.format(join(args))))
 
 @run_async
 def zeroes(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
-    message.reply_text(newton.zeroes('{}'.format(args[0])))
+    message.reply_text(newton.zeroes('{}'.format(join(args))))
 
 @run_async
 def tangent(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
-    message.reply_text(newton.tangent('{}'.format(args[0])))
+    message.reply_text(newton.tangent('{}'.format(join(args))))
 
 @run_async
 def area(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
-    message.reply_text(newton.area('{}'.format(args[0])))
+    message.reply_text(newton.area('{}'.format(join(args))))
 
 @run_async
-def cos(bot: Bot, update: Update, args: List[str]):
+def cos(bot: Bot, update: Update, args):
     message = update.effective_message
-    message.reply_text(newton.cos('{}'.format(args[0])))
+    message.reply_text(math.cos(int(args[0])))
 
 @run_async
-def sin(bot: Bot, update: Update, args: List[str]):
+def sin(bot: Bot, update: Update, args):
     message = update.effective_message
-    message.reply_text(newton.sin('{}'.format(args[0])))
+    message.reply_text(math.sin(int(args[0])))
 
 @run_async
-def tan(bot: Bot, update: Update, args: List[str]):
+def tan(bot: Bot, update: Update, args):
     message = update.effective_message
-    message.reply_text(newton.tan('{}'.format(args[0])))
+    message.reply_text(math.tan(int(args[0])))
 
 @run_async
-def arccos(bot: Bot, update: Update, args: List[str]):
+def arccos(bot: Bot, update: Update, args):
     message = update.effective_message
-    message.reply_text(newton.arccos('{}'.format(args[0])))
+    message.reply_text(math.acos(int(args[0])))
 
 @run_async
-def arcsin(bot: Bot, update: Update, args: List[str]):
+def arcsin(bot: Bot, update: Update, args):
     message = update.effective_message
-    message.reply_text(newton.arcsin('{}'.format(args[0])))
+    message.reply_text(math.asin(int(args[0])))
 
 @run_async
-def arctan(bot: Bot, update: Update, args: List[str]):
+def arctan(bot: Bot, update: Update, args):
     message = update.effective_message
-    message.reply_text(newton.arctan('{}'.format(args[0])))
+    message.reply_text(math.atan(int(args[0])))
 
 @run_async
-def abs(bot: Bot, update: Update, args: List[str]):
+def abs(bot: Bot, update: Update, args):
     message = update.effective_message
-    message.reply_text(newton.abs('{}'.format(args[0])))
+    message.reply_text(math.fabs(int(args[0])))
 
 @run_async
-def log(bot: Bot, update: Update, args: List[str]):
+def log(bot: Bot, update: Update, args):
     message = update.effective_message
-    message.reply_text(newton.log('{}'.format(args[0])))
+    message.reply_text(math.log(int(args[0])))
 
 __help__ = """
 Solves complex math problems using https://newton.now.sh
-
  - /simplify: Simplify `/simplify 2^2+2(2)`
  - /factor: Factor `/factor x^2 + 2x`
  - /derive: Derive `/derive x^2+2x`
@@ -100,11 +103,8 @@ Solves complex math problems using https://newton.now.sh
  - /arctan: Inverse Tangent `/arctan 0`
  - /abs: Absolute Value `/abs -1`
  - /log: Logarithm `/log 2l8`
-
 __Keep in mind__: To find the tangent line of a function at a certain x value, send the request as c|f(x) where c is the given x value and f(x) is the function expression, the separator is a vertical bar '|'. See the table above for an example request.
-
 To find the area under a function, send the request as c:d|f(x) where c is the starting x value, d is the ending x value, and f(x) is the function under which you want the curve between the two x values.
-
 To compute fractions, enter expressions as numerator(over)denominator. For example, to process 2/4 you must send in your expression as 2(over)4. The result expression will be in standard math notation (1/2, 3/4).
 """
 
