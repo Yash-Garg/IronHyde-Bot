e8f9080e341363a9d56103e625c94b6c73268f89
Yash-Garg <ben10.yashgarg@gmail.com>
2018-08-04 21:54:33 +0530

    Change the gban message
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>

--- IHbot/modules/global_bans.py
+++ IHbot/modules/global_bans.py
@@ -91,7 +91,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
 
         return
 
-    message.reply_text("*Blows dust off of banhammer* 😉")
+    message.reply_text("*Summoning the ban axe* 😉")
 
     banner = update.effective_user  # type: Optional[User]
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
