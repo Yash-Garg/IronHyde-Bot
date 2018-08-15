c9a55e9819f000a46539a84789c0e2015e64d69e
Paul Larsen <pn1315@ic.ac.uk>
2018-08-15 23:41:43 +0100

    Fix a bug in Unmuting and AFK

--- IHbot/modules/afk.py
+++ IHbot/modules/afk.py
@@ -40,8 +40,8 @@ def no_longer_afk(bot: Bot, update: Update):
 @run_async
 def reply_afk(bot: Bot, update: Update):
     message = update.effective_message  # type: Optional[Message]
-    if message.entities and message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION]):
-        entities = message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
+    entities = message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
+    if message.entities and entities:
         for ent in entities:
             if ent.type == MessageEntity.TEXT_MENTION:
                 user_id = ent.user.id
@@ -67,14 +67,13 @@ def reply_afk(bot: Bot, update: Update):
 
 def check_afk(bot, update, user_id, fst_name):
     if sql.is_afk(user_id):
-        user = sql.check_afk_status(user_id)
-        if not user.reason:
-            res = "{} is AFK!".format(fst_name)
-        else:
-            res = "{} is AFK! says it because : \n{}".format(fst_name, user.reason)
-        update.effective_message.reply_text(res)
-
-
+        valid, reason = sql.check_afk_status(user_id)
+        if valid:
+            if not reason:
+                res = "{} is AFK!".format(fst_name)
+            else:
+                res = "{} is AFK! says its because of:\n{}".format(fst_name, reason)
+            message.reply_text(res)
 
 def __gdpr__(user_id):
     sql.rm_afk(user_id)
