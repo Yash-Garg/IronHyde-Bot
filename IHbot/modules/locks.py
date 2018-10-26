8285a08a03cfb3df29ca7e93aa3eb75c8a5e3246
Peter Parker <igunublue@protonmail.com>
2018-10-26 07:04:05 -0500

    add videonote lock
    run the following in psql to add the necessary columns for videonote locks.
    ```
    alter table permissions add column videonote bool default false;
    ```

--- IHbot/modules/locks.py
+++ IHbot/modules/locks.py
@@ -23,6 +23,7 @@ LOCK_TYPES = {'sticker': Filters.sticker,
               'voice': Filters.voice,
               'document': Filters.document,
               'video': Filters.video,
+              'videonote': Filters.video_note,
               'contact': Filters.contact,
               'photo': Filters.photo,
               'gif': Filters.document & CustomFilters.mime_type("video/mp4"),
@@ -35,7 +36,7 @@ LOCK_TYPES = {'sticker': Filters.sticker,
 
 GIF = Filters.document & CustomFilters.mime_type("video/mp4")
 OTHER = Filters.game | Filters.sticker | GIF
-MEDIA = Filters.audio | Filters.document | Filters.video | Filters.voice | Filters.photo
+MEDIA = Filters.audio | Filters.document | Filters.video | Filters.video_note | Filters.voice | Filters.photo
 MESSAGES = Filters.text | Filters.contact | Filters.location | Filters.venue | Filters.command | MEDIA | OTHER
 PREVIEWS = Filters.entity("url")
 
@@ -250,6 +251,7 @@ def build_lock_message(chat_id):
                    "\n - voice = `{}`" \
                    "\n - document = `{}`" \
                    "\n - video = `{}`" \
+                   "\n - videonote = `{}`" \
                    "\n - contact = `{}`" \
                    "\n - photo = `{}`" \
                    "\n - gif = `{}`" \
@@ -258,7 +260,7 @@ def build_lock_message(chat_id):
                    "\n - forward = `{}`" \
                    "\n - game = `{}`" \
                    "\n - location = `{}`".format(locks.sticker, locks.audio, locks.voice, locks.document,
-                                                 locks.video, locks.contact, locks.photo, locks.gif, locks.url,
+                                                 locks.video, locks.videonote, locks.contact, locks.photo, locks.gif, locks.url,
                                                  locks.bots, locks.forward, locks.game, locks.location)
         if restr:
             res += "\n - messages = `{}`" \
