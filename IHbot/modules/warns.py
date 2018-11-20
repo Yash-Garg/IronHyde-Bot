16358d7f40036384b6d86812854dd6d0f52f467b
Yash-Garg <ben10.yashgarg@gmail.com>
2018-11-20 08:22:23 +0000

    warns : Add admin only in remove warn
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>

--- IHbot/modules/warns.py
+++ IHbot/modules/warns.py
@@ -64,7 +64,7 @@ def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = N
 
     else:
         keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Remove warn", callback_data="rm_warn({})".format(user.id))]])
+            [[InlineKeyboardButton("Remove warn (Admin Only)", callback_data="rm_warn({})".format(user.id))]])
 
         reply = "{} has {}/{} warnings... watch out!".format(mention_html(user.id, user.first_name), num_warns,
                                                              limit)
